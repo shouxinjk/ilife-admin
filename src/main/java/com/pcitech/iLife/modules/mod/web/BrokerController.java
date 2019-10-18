c2b5af2459983bcf8f1010721c6700701ae64a00
Alex Chew <alex_chew@msn.com>
2019-10-18 09:55:30 +0800

    move rest api location

--- src/main/java/com/pcitech/iLife/modules/mod/web/BrokerController.java
+++ src/main/java/com/pcitech/iLife/modules/mod/web/BrokerController.java
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -132,5 +133,73 @@ public class BrokerController extends BaseController {
 			mapList.add(map);
 		}
 		return mapList;
+	}
+	
+	
+	/**
+	 * 根据id获取下级达人列表
+	 */
+	@ResponseBody
+	@RequestMapping(value = "rest/{id}", method = RequestMethod.GET)
+	public List<Map<String, Object>> listBrokers(@PathVariable String id, HttpServletRequest request, HttpServletResponse response, Model model) {
+		List<Map<String, Object>> mapList = Lists.newArrayList();
+		Broker parent = new Broker();
+		parent.setId(id);
+		Broker broker = new Broker();
+		broker.setParent(parent);
+		List<Broker> list =brokerService.findList(broker);
+		for (int i=0; i<list.size(); i++){
+			Broker e = list.get(i);
+			Map<String, Object> map = Maps.newHashMap();
+			map.put("id", e.getId());
+			map.put("name", e.getName());
+			map.put("phone", e.getPhone());
+			map.put("level", e.getLevel());
+			map.put("upgrade", e.getUpgrade());
+			mapList.add(map);
+		}
+		return mapList;
+	}
+	
+	/**
+	 * 给指定ID的达人添加下级达人
+	 */
+	@ResponseBody
+	@RequestMapping(value = "rest/{id}", method = RequestMethod.POST)
+	public Map<String, Object> registerBroker(@PathVariable String id,@RequestBody Broker broker, HttpServletRequest request, HttpServletResponse response, Model model) {
+		Map<String, Object> result = Maps.newHashMap();
+		Broker parent = brokerService.get(id);
+		if(parent == null) {
+			result.put("status",false);
+			result.put("description","Cannot find parent broker by id:"+id);
+		}else {
+			broker.setParent(parent);
+			brokerService.save(broker);
+			result.put("status",true);
+			result.put("description","Broker created successfully");
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * 修改达人：包括修改单个属性如“升级状态”
+	 */
+	@ResponseBody
+	@RequestMapping(value = "rest/{id}", method = RequestMethod.PATCH)
+	public Map<String, Object> modifyBroker(@PathVariable String id,@RequestBody Broker broker, HttpServletRequest request, HttpServletResponse response, Model model) {
+		Map<String, Object> result = Maps.newHashMap();
+		Broker old = brokerService.get(id);
+		if(old == null) {
+			result.put("status",false);
+			result.put("description","Broker does not exist. id:"+id);
+		}else {
+			broker.setParent(old.getParent());
+			broker.setId(old.getId());//使用路径ID替换传递的id参数，避免误操作
+			brokerService.save(broker);
+			result.put("status",true);
+			result.put("description","Broker modified successfully");
+		}
+		return result;
 	}
 }
