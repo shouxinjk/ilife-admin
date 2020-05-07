8cbe7af9b2d5fe31254284e8a9cb9996916b0174
Alex Chew <alex_chew@msn.com>
2020-05-07 14:49:11 +0800

    send msg to marked-value topic

--- src/main/java/com/pcitech/iLife/util/Transfer.java
+++ src/main/java/com/pcitech/iLife/util/Transfer.java
@@ -8,17 +8,29 @@ import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.google.gson.Gson;
 import com.pcitech.iLife.common.utils.KafkaUtils;
+import com.pcitech.iLife.modules.mod.entity.Measure;
 import com.pcitech.iLife.modules.mod.entity.Occasion;
+import com.pcitech.iLife.modules.mod.service.MeasureService;
+import com.pcitech.iLife.modules.ope.entity.Performance;
 
 @Component
 @Aspect
 public class Transfer {
-	private Logger kafka = LoggerFactory.getLogger("kafkaLogger");//kafka output
+	private Logger occasionKafkaLogger = LoggerFactory.getLogger("kafkaLoggerOccasion");//kafka output
+	private Logger valueKafkaLogger = LoggerFactory.getLogger("kafkaLoggerMarkedValue");//kafka output
 //	private Logger logger = LoggerFactory.getLogger(Transfer.class); //file output
+	@Autowired
+	MeasureService measureService;
+	
+	/**
+	 * 在保存Occasion时推送消息
+	 * @param occasion
+	 */
 	//TODO 对于新建内容无法获取id，需要使用Around或After进行切片
     //@Before("execution(* com.pcitech.iLife.modules.mod.service.OccasionService.save(..)) && args(occasion)")
     public void saveOccasion(Occasion occasion) {
@@ -37,8 +49,32 @@ public class Transfer {
 		map.put("expression", occasion.getExpression());
 //		boolean async = false;
 //		KafkaUtils.send(async,gson.toJson(map));
-        kafka.info(gson.toJson(map));
+		occasionKafkaLogger.info(gson.toJson(map));
 //        logger.debug(gson.toJson(map));
         
+    }
+    
+    /**
+     * 在保存商品标注值时推送到分析库。
+     * @param performance
+     */
+    
+    @Before("execution(* com.pcitech.iLife.modules.ope.service.PerformanceService.save(..)) && args(performance)")
+    public void savePerformance(Performance performance) {
+    		Measure m = measureService.get(performance.getMeasure().getId());
+        Gson gson = new Gson();
+		Map map = new HashMap();
+		map.put("type", "dic");//dic 、 ref，分别表示来源于引用或字典
+		map.put("category", m.getCategory().getId());
+		map.put("property_key", m.getProperty());
+		map.put("property", m.getName());
+		map.put("org_value", performance.getOriginalValue());
+		map.put("mark_type", "manual");//manual、auto
+		map.put("score", performance.getMarkedValue());
+		map.put("rank", performance.getLevel());
+		map.put("status", "ready");
+		map.put("createdOn",performance.getCreateDate());
+		map.put("modifiedOn", new Date());
+		valueKafkaLogger.info(gson.toJson(map));
     }
 }
