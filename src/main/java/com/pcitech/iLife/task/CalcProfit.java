cecc38eedefd9f989d1e4b89167ade92c022f930
Alex Chew <alex_chew@msn.com>
2021-08-24 23:24:46 +0800

    bug fix

--- src/main/java/com/pcitech/iLife/task/CalcProfit.java
+++ src/main/java/com/pcitech/iLife/task/CalcProfit.java
@@ -137,7 +137,7 @@ public class CalcProfit {
 	//        		+ "doc.source in "+JSON.toJSONString(source)+" and "//注意：slow query。需要逐个查询
 				+ "doc.source == \""+s+"\" and "        		
 //	        		+ "IS_NUMBER(doc.price.bid)  and " //注意：在使用函数时不能使用索引，导致slow query
-					+ "doc.price.bid!=null " //仅对于有price.bid才处理
+					+ "doc.price.bid!=null " //仅对于有price.bid才处理 
 //	        		+ "(doc.profit == null || doc.profit.type == null || (doc.profit.type != \"2-party\"  && doc.profit.type != \"3-party\")) "
 	        		+ "doc.status.monetize == \"pending\" "//根据状态查询，优先使用枚举值使用索引查询，需要同时完成对初始profit.type的设置
 	        		+ "update doc with {status:{monetize:\"ready\"}} in my_stuff "
--- src/main/java/com/pcitech/iLife/task/CalcProfit2Party.java
+++ src/main/java/com/pcitech/iLife/task/CalcProfit2Party.java
@@ -132,7 +132,7 @@ public class CalcProfit2Party {
 //	        		+ "doc.source in "+JSON.toJSONString(source)+" and "//注意：slow query。需要逐个查询
 				+ "doc.source == \""+s+"\" and "
 	        		+ "doc.profit.type == \"2-party\" and doc.profit.amount != null and doc.price.sale != null "
-	        		+ "limit 1000 "//一个批次处理100条
+	        		+ "limit 1000 "//一个批次处理100条 
 	        		+ "return {itemKey:doc._key,source:doc.source,category:doc.categoryId==null?\"\":doc.categoryId,price:doc.price.sale,amount:doc.profit.amount}";
 	        logger.error("try to query pending 2-party items.[query]"+query);
 	        try {
