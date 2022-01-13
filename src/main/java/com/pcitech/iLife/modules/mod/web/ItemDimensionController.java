e72ddbf7d0134bdc239bf30ab11431e6307236f8
Alex Chew <alex_chew@msn.com>
2022-01-13 16:56:16 +0800

    bug fix

--- src/main/java/com/pcitech/iLife/modules/mod/web/ItemDimensionController.java
+++ src/main/java/com/pcitech/iLife/modules/mod/web/ItemDimensionController.java
@@ -510,9 +510,10 @@ public class ItemDimensionController extends BaseController {
 	
 	//动态计算脚本并保存
 	private void saveWithScript(ItemDimension itemDimension) {
+		logger.error("try to save with script.[itemDimension.id]"+itemDimension.getId(),itemDimension);
 		//预生成脚本：对于weighted-sum脚本，自动查询下级节点，并生成
 		if(!itemDimension.getIsNewRecord() && itemDimension.getId()!=null && itemDimension.getId().trim().length()>0 //对于已经存在的节点进行。新节点无需处理
-				&& itemDimension.getScript()!=null||itemDimension.getScript().trim().length()>0||itemDimension.getScript().indexOf("weighted-sum")>=0) {
+				&& itemDimension.getScript()!=null && itemDimension.getScript().trim().length()>0 && itemDimension.getScript().indexOf("weighted-sum")>=0) {
 			//先获取属性列表
 			ItemDimensionMeasure itemDimensionMeasure = new ItemDimensionMeasure();
 			itemDimensionMeasure.setDimension(itemDimension);
