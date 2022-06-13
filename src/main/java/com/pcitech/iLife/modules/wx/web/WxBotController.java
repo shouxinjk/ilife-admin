91ed0e33e0623dc3a5262602a89b449372d65cee
Alex Chew <alex_chew@msn.com>
2022-06-13 15:39:53 +0800

    add rest api:s

--- src/main/java/com/pcitech/iLife/modules/wx/web/WxBotController.java
+++ src/main/java/com/pcitech/iLife/modules/wx/web/WxBotController.java
@@ -3,6 +3,10 @@
  */
 package com.pcitech.iLife.modules.wx.web;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -11,16 +15,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.pcitech.iLife.common.config.Global;
 import com.pcitech.iLife.common.persistence.Page;
 import com.pcitech.iLife.common.web.BaseController;
 import com.pcitech.iLife.common.utils.StringUtils;
+import com.pcitech.iLife.modules.mod.entity.Broker;
+import com.pcitech.iLife.modules.mod.service.BrokerService;
 import com.pcitech.iLife.modules.wx.entity.WxBot;
+import com.pcitech.iLife.modules.wx.entity.WxGroupTask;
 import com.pcitech.iLife.modules.wx.service.WxBotService;
+import com.pcitech.iLife.util.Util;
 
 /**
  * 微信机器人Controller
@@ -33,6 +48,8 @@ public class WxBotController extends BaseController {
 
 	@Autowired
 	private WxBotService wxBotService;
+	@Autowired
+	private BrokerService brokerService;
 	
 	@ModelAttribute
 	public WxBot get(@RequestParam(required=false) String id) {
@@ -78,6 +95,75 @@ public class WxBotController extends BaseController {
 		wxBotService.delete(wxBot);
 		addMessage(redirectAttributes, "删除微信机器人成功");
 		return "redirect:"+Global.getAdminPath()+"/wx/wxBot/?repage";
+	}
+	
+	/**
+	 * 根据brokerId获取指定达人已开通机器人
+	 */
+	@ResponseBody
+	@RequestMapping(value = "rest/byBrokerId/{brokerId}", method = RequestMethod.GET)
+	public Map<String,Object> getBotByBrokerId(@PathVariable String brokerId) {
+		Map<String,Object> result = Maps.newHashMap();
+		result.put("success", false);
+		Broker broker = brokerService.get(brokerId);
+		WxBot wxBot = new WxBot();
+		if(broker==null) {//如果为空则返回空白
+			result.put("msg", "cannot find broker by id:"+brokerId);
+			return result;
+		}
+		wxBot.setBroker(broker);
+		List<WxBot> wxBots = wxBotService.findList(wxBot);
+		if(wxBots.size()>0) {
+			result.put("success", true);
+			result.put("data", wxBots.get(0));
+		}else {
+			result.put("msg", "no bot found by broker id:"+brokerId);
+		}
+		return result;
+	}
+	
+	/**
+	 * 根据brokerId请求开通机器人
+	 */
+	@ResponseBody
+	@RequestMapping(value = "rest/botByBrokerId/{brokerId}", method = RequestMethod.POST)
+	public Map<String,Object> createBotByBrokerId(@PathVariable String brokerId, @RequestBody JSONObject json) {
+		Map<String,Object> result = Maps.newHashMap();
+		result.put("success", false);
+		Broker broker = brokerService.get(brokerId);
+		WxBot wxBot = new WxBot();
+		if(broker==null) {//如果为空则返回空白
+			result.put("msg", "cannot find broker by id:"+brokerId);
+			return result;
+		}
+		wxBot.setBroker(broker);
+		List<WxBot> wxBots = wxBotService.findList(wxBot);
+		if(wxBots.size()>0) {//如果有就直接返回
+			result.put("success", true);
+			result.put("data", wxBots.get(0));
+			result.put("msg", "got existed bot.");
+		}else {//否则新建一个
+			//在broker上生成激活码
+			String code = Util.get6bitCode(brokerId);//根据达人ID生成
+			broker.setToken(code);
+			brokerService.save(broker);
+			//新建bot
+			String botId = Util.get32UUID();
+			wxBot.setId(botId);
+			wxBot.setIsNewRecord(true);
+			wxBot.setStatus("inactive");
+			wxBot.setName("BOT-"+broker.getNickname());
+			wxBot.setType(json.getString("type")!=null?json.getString("type"):"web");//默认设为web
+			wxBot.setEffectFrom(json.getDate("effectFrom")!=null?json.getDate("effectFrom"):new Date());
+			wxBot.setExpireOn(json.getDate("expireOn")!=null?json.getDate("expireOn"):null);
+			wxBot.setCreateDate(new Date());
+			wxBot.setUpdateDate(new Date());
+			wxBotService.save(wxBot);
+			result.put("success", true);
+			result.put("data", wxBotService.get(botId));
+			result.put("msg", "new bot created successfully.");
+		}
+		return result;
 	}
 
 }
--- src/main/java/com/pcitech/iLife/modules/wx/web/WxGroupTaskController.java
+++ src/main/java/com/pcitech/iLife/modules/wx/web/WxGroupTaskController.java
@@ -3,6 +3,7 @@
  */
 package com.pcitech.iLife.modules.wx.web;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -14,20 +15,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.pcitech.iLife.common.config.Global;
 import com.pcitech.iLife.common.persistence.Page;
 import com.pcitech.iLife.common.web.BaseController;
 import com.pcitech.iLife.common.utils.StringUtils;
 import com.pcitech.iLife.modules.mod.entity.Broker;
+import com.pcitech.iLife.modules.mod.service.BrokerService;
+import com.pcitech.iLife.modules.wx.entity.WxBot;
 import com.pcitech.iLife.modules.wx.entity.WxGroupTask;
 import com.pcitech.iLife.modules.wx.service.WxGroupTaskService;
+import com.pcitech.iLife.util.Util;
 
 /**
  * 微信群任务Controller
@@ -40,6 +48,8 @@ public class WxGroupTaskController extends BaseController {
 
 	@Autowired
 	private WxGroupTaskService wxGroupTaskService;
+	@Autowired
+	private BrokerService brokerService;
 	
 	@ModelAttribute
 	public WxGroupTask get(@RequestParam(required=false) String id) {
@@ -89,11 +99,63 @@ public class WxGroupTaskController extends BaseController {
 	
 	/**
 	 * 根据nickname获取指定达人微信群任务
+	 * 仅返回active任务
 	 */
 	@ResponseBody
 	@RequestMapping(value = "rest/byNickname", method = RequestMethod.GET)
 	public List<WxGroupTask> getBrokerByNickname(@RequestParam(required=true) String nickname) {
 		 return wxGroupTaskService.getByNickname(nickname);//根据nickname获取达人
 	}
+	
+	/**
+	 * 根据达人ID获取所有微信群任务，包括active及inactive
+	 */
+	@ResponseBody
+	@RequestMapping(value = "rest/byBrokerId/{brokerId}", method = RequestMethod.GET)
+	public List<WxGroupTask> listBrokerGroupTasks(@PathVariable String brokerId) {
+		Broker broker = brokerService.get(brokerId);
+		if(broker==null) {//如果为空则返回空白列表
+			return Lists.newArrayList();
+		}
+		WxGroupTask wxGroupTask = new WxGroupTask();
+		wxGroupTask.setBroker(broker);
+		return wxGroupTaskService.findList(wxGroupTask);//根据brokerId获取列表
+	}
 
+	/**
+	 * 获取指定ID微信群任务
+	 */
+	@ResponseBody
+	@RequestMapping(value = "rest/byId/{id}", method = RequestMethod.GET)
+	public Map<String,Object> getGroupTask(@PathVariable String id) {
+		Map<String,Object> result = Maps.newHashMap();
+		result.put("success", false);
+		WxGroupTask wxGroupTask = wxGroupTaskService.get(id);
+		if(wxGroupTask==null) {
+			result.put("msg", "cannot find group task by id."+id);
+			return result;
+		}
+		result.put("data", wxGroupTask);
+		result.put("success", true);
+		return result;
+	}
+	
+	/**
+	 * 更新微信群任务
+	 */
+	@ResponseBody
+	@RequestMapping(value = "rest/byId/{id}", method = RequestMethod.POST)
+	public Map<String,Object> updateGroupTask(@PathVariable String id, @RequestBody WxGroupTask data) {
+		Map<String,Object> result = Maps.newHashMap();
+		result.put("success", false);
+		if(wxGroupTaskService.get(id)==null) {
+			result.put("msg", "cannot find group task by id."+id);
+			return result;
+		}
+		data.setId(id);//优先采用path指定的id
+		data.setUpdateDate(new Date());
+		wxGroupTaskService.save(data);
+		result.put("success", true);
+		return result;
+	}
 }
