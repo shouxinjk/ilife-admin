9c4433954170f88d7b2a547cfcee889fdd9afe8e
Alex Chew <alex_chew@msn.com>
2023-01-20 11:40:52 +0800

    add rest api

--- src/main/java/com/pcitech/iLife/modules/mod/web/OccasionController.java
+++ src/main/java/com/pcitech/iLife/modules/mod/web/OccasionController.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -163,6 +164,12 @@ public class OccasionController extends BaseController {
 			
 		}
 		return mapList;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "rest/all-occasions")
+	public List<Occasion> listAllOccasions(@RequestBody Occasion occasion) {
+		return occasionService.findList(occasion);
 	}
 	
 	/**
